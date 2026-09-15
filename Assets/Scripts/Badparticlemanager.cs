62b53b98efc3290bb134d503ca8b2f6b07552343
jvogt23 <jamesvogt70@gmail.com>
2026-09-14 23:24:59 -0400

    Added logic to escalate attacks as level progresses - Needs work

--- Assets/Scripts/Badparticlemanager.cs
+++ Assets/Scripts/Badparticlemanager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Badparticlemanager : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class Badparticlemanager : MonoBehaviour
 
     //how many particles 'live'
     public int particleCount = 4;
+    // How many attackers may exist at once
+    public int attackerCount {get; private set;} = 1;
+    // Delay between sending attackers during an individual attack run
+    // TODO: Make a range of min to max following uniform or normal dist
+    // TODO: Make the attack delay and cooldown go down with game stage
+    public float attackerDelay = 0.5f;
 
     //attack timing
     public float minAttackDelay = 3f;
@@ -19,7 +26,8 @@ public class Badparticlemanager : MonoBehaviour
     public float cooldown = 2f;
 
     //particles that currently exist (tracked) 
-    private List<BadParticle> particles = new List<BadParticle>();
+    private List<BadParticle> idleParticles = new List<BadParticle>();
+    private List<BadParticle> attackingParticles = new List<BadParticle>();
 
     public void Start(){
         for (int i = 0; i < particleCount; i++){
@@ -35,13 +43,55 @@ public class Badparticlemanager : MonoBehaviour
             //remember the particle
             newParticle.roomBounds = roomBounds;
             newParticle.playerHead = playerHead;
-            particles.Add(newParticle);
+            idleParticles.Add(newParticle);
         }
+        LevelClock.onStageChange += OnStageChange;
         StartCoroutine(AttackLoop());
     }
+
+    public void SetAttackerCount(int count) {
+        if (count > particleCount) {
+            attackerCount = particleCount;
+            return;
+        }
+        attackerCount = count;
+    }
+
     BadParticle PickAttacker(){
-        if (particles.Count == 0) return null; 
-        return particles[Random.Range(0, particles.Count)]; //pick random particle
+        if (idleParticles.Count == 0) return null;
+        int idx = Random.Range(0, idleParticles.Count);
+        BadParticle particle = idleParticles[idx];
+        idleParticles.RemoveAt(idx);
+        attackingParticles.Add(particle);
+        return particle;
+    }
+
+    private bool AnyAttacking() {
+        return attackingParticles.Any(attacker => attacker.IsAttacking);
+    }
+
+    void OnStageChange(int stage) {
+        if (stage == 4) {
+            cooldown = 0;
+            minAttackDelay = 0;
+            maxAttackDelay = 0;
+        } else {
+            if (minAttackDelay != 0) {
+                minAttackDelay -= 1;
+            }
+            if (maxAttackDelay != 0) {
+                maxAttackDelay -= 2;
+            }
+            if (cooldown != 0) {
+                cooldown -= 0.5f;
+            }
+            SetAttackerCount(attackerCount + 1);
+        }
+    }
+
+    void OnDisable()
+    {
+        LevelClock.onStageChange -= OnStageChange;
     }
 
     IEnumerator AttackLoop(){
@@ -56,11 +106,27 @@ public class Badparticlemanager : MonoBehaviour
             if (attacker == null) {
                 continue;
             }
-            attacker.Attack(playerHead);
-            while (attacker.IsAttacking){
+            StartCoroutine(SendAttackers(attackerCount));
+            while (AnyAttacking()){
                 yield return null;
             }
+            idleParticles.AddRange(attackingParticles);
+            attackingParticles.Clear();
             yield return new WaitForSeconds(cooldown);
+        }
+    }
+
+    IEnumerator SendAttackers(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            BadParticle attacker = PickAttacker();
+            if (attacker == null) {
+                continue;
+            }
+            attacker.Attack(playerHead);
+            if (n > 1 && i < n - 1)
+                yield return new WaitForSeconds(attackerDelay);
         }
     }
     
