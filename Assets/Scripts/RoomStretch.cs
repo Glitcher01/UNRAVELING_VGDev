869526f0ff50a846cd5dae4c3fd46aeebf3ef982
Daniel Feng <dxfeng08@gmail.com>
2026-09-15 09:18:57 -0400

    made walls stretch by scaling

--- Assets/Scripts/RoomStretch.cs
+++ Assets/Scripts/RoomStretch.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
+
 // As the level clock advances into stages 2 and 3, the objects in
 // `objectsToStretch` move backward, making the room feel unnaturally long
 public class RoomStretch : MonoBehaviour
 {
     public Transform[] objectsToStretch;  
-
-    public float stretchPerStage = 3f;    
+    public float stretchPerStage = 4f;    
     public float stretchSpeed = 0.5f;      
 
     private float[] baseZ; 
-    private float targetOffset;             
+    private float targetOffset;   
+
+    // vars needed for walls
+    public Transform[] wallsToStretch;
+    private float[] wallsBaseZ;
+    private float[] wallsBaseScale;
+    private float[] wallsBaseLength;          
 
     void Start()
     {
@@ -18,6 +24,19 @@ public class RoomStretch : MonoBehaviour
         baseZ = new float[objectsToStretch.Length];
         for (int i = 0; i < objectsToStretch.Length; i++)
             baseZ[i] = objectsToStretch[i].position.z;
+        
+        // remember walls start, scaling, and length
+        
+        wallsBaseZ = new float[wallsToStretch.Length];
+        wallsBaseScale = new float[wallsToStretch.Length];
+        wallsBaseLength = new float[wallsToStretch.Length];
+        for (int i = 0; i < wallsToStretch.Length; i++) {
+            Transform walls = wallsToStretch[i];
+            wallsBaseZ[i] = walls.position.z;
+            wallsBaseScale[i] = walls.localScale.y;
+            Renderer r = walls.GetComponent<Renderer>();
+            wallsBaseLength[i] = r.bounds.size.z;     
+        }
 
         targetOffset = 0f;
 
@@ -43,6 +62,22 @@ public class RoomStretch : MonoBehaviour
             float goalZ = baseZ[i] + targetOffset;   // each object's own base + the shared offset
             p.z = Mathf.Lerp(p.z, goalZ, Time.deltaTime * stretchSpeed);
             objectsToStretch[i].position = p;
+        }
+
+        // for stretching wall
+        for (int i = 0; i < wallsToStretch.Length; i++) {
+            Transform walls = wallsToStretch[i];
+            float wallsGoalLength = wallsBaseLength[i] + targetOffset;
+            float wallsGoalScale = wallsBaseScale[i] * (wallsGoalLength / wallsBaseLength[i]);
+            float wallsGoalPos = wallsBaseZ[i] + targetOffset * 0.5f;
+
+            Vector3 s = walls.localScale;
+            s.y = Mathf.Lerp(s.y, wallsGoalScale, Time.deltaTime * stretchSpeed);
+            walls.localScale = s;
+
+            Vector3 p_walls = walls.position;
+            p_walls.z = Mathf.Lerp(p_walls.z, wallsGoalPos, Time.deltaTime * stretchSpeed);
+            walls.position = p_walls;
         }
     }
 }
