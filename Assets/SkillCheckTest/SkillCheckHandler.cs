8f8d054d773e23402dceb1591c1d1b1504b7629a
Glitcher01 <lokesht20077@gmail.com>
2026-09-18 13:42:09 -0400

    Added player interaction, submitting a pull request in.

--- Assets/SkillCheckTest/SkillCheckHandler.cs
+++ Assets/SkillCheckTest/SkillCheckHandler.cs
@@ -1,69 +1,95 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
+
+[System.Serializable]
+public class SkillCheckSettings
+{
+    public float spinnerMult = 1.1f;
+}
 
 public class SkillCheckHandler : MonoBehaviour
 {
-
+    [SerializeField] private SkillCheckSettings skillCheckSettings;
     [Header("State")]
     [SerializeField] protected Vector2 canvasPosition;
-    [SerializeField] protected GameObject background;
-    [SerializeField] protected GameObject zone;
-    [SerializeField] protected GameObject spinner;
+    [SerializeField] protected RectTransform backgroundRT;
+    [SerializeField] protected RectTransform zoneRT;
+    [SerializeField] protected RectTransform spinnerRT;
     [SerializeField] protected float spinnerSpeed;
     [SerializeField] protected float radiusOffSet;
-
     RectTransform rt;
     Material mat;
+
     // Measured in radians, from 0 to 2pi.Also assumes that the radius is zoneRT's width / 2.
-    float startingPosition = 0;
+    float position = 0;
+    int currentMin;
+    int currentMax;
+    bool forward = true;
+    Unity.Mathematics.Random rng;
 
     private void Start()
     {
         rt = GetComponent<RectTransform>();
-        RectTransform backgroundRT = background.GetComponent<RectTransform>();
-        RectTransform zoneRT = zone.GetComponent<RectTransform>();
-        RectTransform spinnerRT = spinner.GetComponent<RectTransform>();
-
         mat = zoneRT.GetComponent<RawImage>().material;
 
-        if (rt == null) {
+        if (rt == null || backgroundRT == null || zoneRT == null || spinnerRT == null) {
             return;
         }
 
-        rt.anchoredPosition =  canvasPosition;
-
-        if (backgroundRT == null || zoneRT == null || spinnerRT == null) {
-            return;
-        }
-
+        rt.anchoredPosition = canvasPosition;
         backgroundRT.anchoredPosition = Vector2.zero;
         zoneRT.anchoredPosition = Vector2.zero;
 
         int textureID = Shader.PropertyToID("_imageTexture");
-        mat.SetTexture(textureID, zone.GetComponent<RawImage>().texture);
+        mat.SetTexture(textureID, zoneRT.GetComponent<RawImage>().texture);
+
+        rng = new Unity.Mathematics.Random((uint)System.DateTime.Now.Ticks);
+
+        currentMin = rng.NextInt(360);
+        Debug.Log(currentMin);
+        currentMax = (currentMin + 30 + 360) % 360;
     }
 
     private void Update()
     {
-        RectTransform spinnerRT = spinner.GetComponent<RectTransform>();
-        RectTransform zoneRT = zone.GetComponent<RectTransform>();
+        visualUpdate();
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            float calcPos = position * 180 / Mathf.PI;
+            if (currentMin < currentMax)
+            {    
+                if (calcPos >= currentMin && calcPos <= currentMax)
+                {
+                    generateSkillZone();
+                }
+            }
+            else if (calcPos >= currentMin || calcPos <= currentMax)
+            {
+                generateSkillZone();
+            }
+        }
+    }
 
+    private void visualUpdate()
+    {
         float radius = zoneRT.rect.width / 2 + radiusOffSet;
-        spinnerRT.anchoredPosition = radius * new Vector2(Mathf.Cos(startingPosition), Mathf.Sin(startingPosition));
-        spinnerRT.rotation = Quaternion.Euler(0, 0, (float)(startingPosition * 180 / Math.PI));
-        startingPosition += spinnerSpeed * Time.deltaTime;
-        startingPosition %= Mathf.PI * 2;
 
+        spinnerRT.anchoredPosition = radius * new Vector2(Mathf.Cos(position), Mathf.Sin(position));
+        spinnerRT.rotation = Quaternion.Euler(0, 0, (float)(position * 180 / Math.PI));
+        position += (forward ? 1 : -1) * spinnerSpeed * Time.deltaTime;
+        position = (position + Mathf.PI * 2) % (Mathf.PI * 2);
 
         int minID = Shader.PropertyToID("_minAngle");
         int maxID = Shader.PropertyToID("_maxAngle");
+        mat.SetFloat(minID, currentMin);
+        mat.SetFloat(maxID, currentMax);
+    }
 
-        mat.SetFloat(minID, 45);
-        mat.SetFloat(maxID, 75);
+    private void generateSkillZone()
+    {
+        forward = !forward;
+        spinnerSpeed *= skillCheckSettings.spinnerMult;
+        currentMin = rng.NextInt(360);
+        currentMax = (currentMin + 30 + 360) % 360;
     }
 }
